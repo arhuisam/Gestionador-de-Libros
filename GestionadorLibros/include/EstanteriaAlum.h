560ee8456c88b218d3bf59e052cd6089e8de1bc4
arhuisam <78871957+arhuisam@users.noreply.github.com>
2021-03-03 19:18:57 -0500

    cambios

---
+++ GestionadorLibros/include/EstanteriaAlum.h
@@ -0,0 +1,16 @@
+#ifndef ESTANTERIAALUM_H
+#define ESTANTERIAALUM_H
+
+
+class EstanteriaAlum
+{
+    public:
+        EstanteriaAlum();
+        virtual ~EstanteriaAlum();
+
+    protected:
+
+    private:
+};
+
+#endif // ESTANTERIAALUM_H
